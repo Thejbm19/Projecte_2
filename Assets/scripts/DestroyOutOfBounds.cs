7185e7d978351c745e983f48d761a5896a542781
Thejbm19 <jaumebauza10@gmail.com>
2021-11-22 10:34:09 +0100

    balas i animal atac

---
+++ Assets/scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+
+    private float upperLim = 30f;
+    private float lowerLim = -10f;
+
+    
+ 
+    void Update()
+    {
+        //bala fallida
+        if (transform.position.z > upperLim)
+        {
+            Destroy(gameObject);
+        }
+
+        //animal fuera de limites
+        if (transform.position.z < lowerLim)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -9,28 +9,38 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     public float xRange = 16;
     public float xRAnge = -16;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
 
+        //limit de pantalla dreta
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+        //limit pantalla esquerra 
+        //if (transform.position.x < -xRange) manera simplificada sa multiplica per -1 sa variable
         if (transform.position.x < xRAnge)
         {
             transform.position = new Vector3(xRAnge, transform.position.y, transform.position.z);
         }
 
+        //disparo
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
